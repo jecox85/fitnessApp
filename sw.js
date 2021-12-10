0360ddff6755beaa6e31908a0f56461c95ee00d6
jecox85 <13830384+jecox85@users.noreply.github.com>
2021-12-10 14:46:25 -0700

    Set up dynamic caching and added fallback page.

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+const dynamicCache = 'Dynamic-cache-v2';
+const staticCache = 'Static-cache-v2';
 const assets = [
     '/',
     '/index.html',
@@ -7,13 +9,13 @@ const assets = [
     '/js/ui.js',
     '/css/app.css',
     '/css/materialize.min.css',
+    '/pages/fallback.html',
     '/pages/about.html',
     '/pages/workoutPage.html',
     '/pages/workoutStart.html',
     '/img/pushup.jpg',
     'https://fonts.googleapis.com/icon?family=Material+Icons'
 ];
-console.log(assets);
 
 self.addEventListener('install', function(event){
     //fires when the browser installs the app.
@@ -21,7 +23,7 @@ self.addEventListener('install', function(event){
     //The purpose of this event is to give the service worker a place to setup the local
     //environment after the installation completes.
     //console.log(`SW: Event fired: ${event.type}`);
-    event.waitUntil(caches.open('static').then(function(cache){
+    event.waitUntil(caches.open(staticCache).then(function(cache){
         //console.log('SW: Precaching App shell.');
         cache.addAll(assets);
     }))
@@ -30,13 +32,24 @@ self.addEventListener('activate', function(event){
     //Fires after the service worker completes its installation.
     //It's a place for the service worker to clean up from previous service worker versions.
     //console.log(`SW: Event fired: ${event.type}`);
+    event.waitUntil(caches.keys().then(keys => {
+        return Promise.all(keys.filter(key => key !== staticCache).map(key => caches.delete(key)));
+    }));
 });
 self.addEventListener('fetch', function(event){
     //fires whenever the app requests a resource (file or data)
     //console.log(`SW: Fetching ${event.request.url}`);
     //next, go get the requested resource from the network.
-    event.respondWith(fetch(event.request));
-    caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-    })
-})
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return (
+                response || fetch(event.request).then((fetchRes) => {
+                    return caches.open(dynamicCache).then((cache) =>{
+                        cache.put(event.request.url, fetchRes.clone());
+                        return fetchRes;
+                    });
+                })
+            );
+        }).catch(() => caches.match('/pages/fallback.html'))
+    );  
+});
