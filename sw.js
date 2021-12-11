e4f8360ac2385c5f142c3f6cb259f01932840d98
jecox85 <13830384+jecox85@users.noreply.github.com>
2021-12-11 08:09:58 -0700

    small update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const dynamicCache = 'Dynamic-cache-v7';
-const staticCache = 'Static-cache-v7';
+const dynamicCache = 'Dynamic-cache-v8';
+const staticCache = 'Static-cache-v8';
 const assets = [
     '/',
     '/index.html',
